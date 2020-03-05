0fc697cee5c8bc83f7a5e6e641064057ac7fc604
전재욱 <jetacore9219@gmail.com>
2020-03-05 18:45:03 +0900

    initailize project

---
+++ back_end/src/main/java/com/delusidiot/blog/delusidiot_blog/DelusidiotBlogApplication.java
@@ -0,0 +1,13 @@
+package com.delusidiot.blog.delusidiot_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelusidiotBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelusidiotBlogApplication.class, args);
+    }
+
+}
---
+++ back_end/src/test/java/com/delusidiot/blog/delusidiot_blog/DelusidiotBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delusidiot.blog.delusidiot_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelusidiotBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
